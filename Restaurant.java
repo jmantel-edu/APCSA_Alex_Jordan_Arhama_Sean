92571c5e8109369deca13b3f09ca79cb6fd77e80
jmantel-edu <jmantel@my.waketech.edu>
2026-09-15 17:00:18 +0000

    Add placeholder functions and structures for the classes

--- Restaurant.java
+++ Restaurant.java
@@ -1,3 +1,20 @@
 public class Restaurant {
+    private String name;
+    private String bio;
+    private float reviews;
+    private int priceRange;
+    private String primaryCategory;
+    private String secondaryCategory;
 
+    public String toString() {
+        return("PLACEHOLDER");
+    }
+
+    public String getSummary() {
+        return("PLACEHOLDER");
+    }
+
+    public String getName() {
+        return(name);
+    }
 }
--- RestaurantRunner.java
+++ RestaurantRunner.java
@@ -1,4 +1,16 @@
 public class RestaurantRunner {
+    private String username;
+
+/*     
+    public Restaurant pickRandomRestaurant() {
+        return("PLACEHOLDER");
+    } 
+
+    public Restaurant[] pickRestaurants(String primary, String secondary, int priceRange) {
+        return("PLACEHOLDER")
+    }
+        
+*/
     public static void main(String[] args) {
         
     }
